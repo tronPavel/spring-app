0f7635568cad53f84397a5363f975ec840dff25e
Lenovo <tronpavel184@gmail.com>
2025-05-24 20:34:39 +0300

    init, connect logger

---
+++ src/main/java/org/example/firstspringapp/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package org.example.firstspringapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/").permitAll() // Разрешить доступ к "/" без аутентификации
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll()
+                )
+                .logout(logout -> logout.permitAll());
+        return http.build();
+    }
+}
---
+++ src/main/java/org/example/firstspringapp/controller/HomeController.java
@@ -0,0 +1,17 @@
+package org.example.firstspringapp.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @GetMapping("/")
+    public String helloWorld() {
+        logger.info("Received request to /");
+        return "Hello, World!";
+    }
+}
